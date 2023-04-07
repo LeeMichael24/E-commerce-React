58081c0d6983f722af7d6f2e8d8a9b72e17c036e
Michael Narváez <82190072+LeeMichael24@users.noreply.github.com>
2023-04-07 02:53:08 -0600

    Add: some error fixed

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,30 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import "./Category.scss";
 
-import cat1 from "../../../assets/category/cat-1.jpg"
-import cat2 from "../../../assets/category/cat-2.jpg"
-import cat3 from "../../../assets/category/cat-3.jpg"
-import cat4 from "../../../assets/category/cat-4.jpg"
-
-
-const Category = () => {
+const Category = ({ categories }) => {
+    const navigate = useNavigate();
     return (
         <div className="shop-by-category">
             <div className="categories">
-                <div className="category">
-                    <img src={ cat1 } alt=""/>
-                </div>
-                <div className="category">
-                    <img src={ cat2 } alt=""/>
-                </div>
-                <div className="category">
-                    <img src={ cat3 } alt=""/>
-                </div>
-                <div className="category">
-                    <img src={ cat4 } alt=""/>
-                </div>
+                {categories?.data?.map((item) => (
+                    <div
+                        key={item.id}
+                        className="category"
+                        onClick={() => navigate(`/category/${item.id}`)}
+                    >
+                        <img
+                            src={
+                                process.env.REACT_APP_STRIPE_APP_DEV_URL +
+                                item.attributes.img.data.attributes.url
+                            } alt=""
+                        />
+                    </div>
+                ))}
             </div>
         </div>
-    )
-} 
+    );
+};
 
 export default Category;
