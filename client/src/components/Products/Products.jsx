98e402e98aff020f7d15b4e67396f35f23845cf9
Michael Narváez <82190072+LeeMichael24@users.noreply.github.com>
2023-04-07 03:01:32 -0600

    Add: related products for details page

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,24 +1,21 @@
 import "./Products.scss";
+import Product from "./Product/Product";
 
-import Product from "./Product/Product"
-
-const Products = ( {innerPage }) => {
+const Products = ({ products, innerPage, headingText }) => {
     return (
         <div className="products-container">
-            { !innerPage && <div className="sec-heading"> Section Heading</div>}
-            <div className="products">
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
+            {!innerPage && <div className="sec-heading">{headingText}</div>}
+            <div className={`products ${innerPage ? "innerPage" : ""}`}>
+                {products?.data?.map((item) => (
+                    <Product
+                        key={item.id}
+                        id={item.id}
+                        data={item.attributes}
+                    />
+                ))}
             </div>
         </div>
-    )
+    );
 };
 
 export default Products;
