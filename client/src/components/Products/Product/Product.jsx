d6c9e26c1f0675e105a51b1e83b4669b5e001642
Michael Narváez <82190072+LeeMichael24@users.noreply.github.com>
2023-04-07 02:58:24 -0600

    Add: product detail page

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,14 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Product.scss";
 
-import prod from "../../../assets/products/earbuds-prod-1.webp"
-const Product = () => {
+const Product = ({ data, id }) => {
+    const navigate = useNavigate();
     return (
-        <div className="product-card">
+        <div
+            className="product-card"
+            onClick={() => navigate("/product/" + id)}
+        >
             <div className="thumbnail">
-                <img src={ prod } alt=""/>
+                <img
+                    src={
+                        process.env.REACT_APP_STRIPE_APP_DEV_URL +
+                        data.image.data[0].attributes.url
+                    } alt=""
+                />
             </div>
             <div className="prod-details">
-                <span className="name"> Product name </span>
+                <span className="name"> { data.title } </span>
                 <span className="price"> &euro;499 </span>
             </div>
         </div>
