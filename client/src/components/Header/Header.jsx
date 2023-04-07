06a2a2868d1ff347d2bb6100c3ed75f7565d890d
Michael Narváez <82190072+LeeMichael24@users.noreply.github.com>
2023-04-07 03:06:00 -0600

    Add: cart & search bar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,52 +1,63 @@
 import { useEffect, useState, useContext } from "react";
-import { Navigate } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
-
-import { Search } from "./Search/Search";
-import Cart from "../Cart/Cart";
-import { Context } from "../../utils/context";
-
 import "./Header.scss";
+import Search from "./Search/Search";
+import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
 
 const Header = () => {
-  const [scrolled, setScreolled] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
+    const [searchModal, setSearchModal] = useState(false);
+    const navigate = useNavigate();
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 200) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    };
 
-  const handleScroll = () => {
-    const offset = window.scrollY;
-    if (offset > 200) {
-      setScreolled(true);
-    } else {
-      setScreolled(false);
-    }
-  };
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    }, []);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-  }, []);
+    const { cartCount, showCart, setShowCart } = useContext(Context);
 
-  return (
-    <header className={`main-header ${scrolled ? "sticky-header" : ""} `}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
-        <div className="center"> AppEx </div>
-        <div className="right">
-          <TbSearch />
-          <AiOutlineHeart />
-          <span className="cart-icon">
-            <CgShoppingCart />
-            <span> 5 </span>
-          </span>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <>
+            <header
+                className={`main-header ${scrolled ? "sticky-header" : ""}`}
+            >
+                <div className="header-content">
+                    <ul className="left">
+                        <li onClick={() => navigate("/")}>Home</li>
+                        <li onClick={() => navigate("/about")}>About</li>
+                        <li>Categories</li>
+                    </ul>
+                    <div className="center" onClick={() => navigate("/")}>
+                        AppEx. 
+                    </div>
+                    <div className="right">
+                        <TbSearch onClick={() => setSearchModal(true)} />
+                        <AiOutlineHeart />
+                        <span
+                            className="cart-icon"
+                            onClick={() => setShowCart(true)}
+                        >
+                            <CgShoppingCart />
+                            {!!cartCount && <span>{cartCount}</span>}
+                        </span>
+                    </div>
+                </div>
+            </header>
+            {searchModal && <Search setSearchModal={setSearchModal} />}
+            {showCart && <Cart />}
+        </>
+    );
 };
 
 export default Header;
