8e4894e663c46a37d29f845bd75ceb8e5df6f9a4
Michael Narváez <82190072+LeeMichael24@users.noreply.github.com>
2023-04-07 03:17:02 -0600

    Add: some small changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,29 @@
-import { useEffect } from "react";
-import { createContext, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
 
-export const Context = createContext();
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+import AppContext from "./utils/context";
 
-const AppContext = ({ children }) => {
-    const [categories, setCategories] = useState();
-    const [products, setProducts] = useState();
-    const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    const [cartCount, setCartCount] = useState(0);
-    const [cartSubTotal, setCartSubTotal] = useState(0);
-    const location = useLocation();
-
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [location]);
-
-    useEffect(() => {
-        let count = 0;
-        cartItems?.map((item) => (count += item.attributes.quantity));
-        setCartCount(count);
-
-        let subTotal = 0;
-        cartItems.map(
-            (item) =>
-                (subTotal += item.attributes.price * item.attributes.quantity)
-        );
-        setCartSubTotal(subTotal);
-    }, [cartItems]);
-
-    const handleAddToCart = (product, quantity) => {
-        let items = [...cartItems];
-        let index = items?.findIndex((p) => p.id === product?.id);
-        if (index !== -1) {
-            items[index].attributes.quantity += quantity;
-        } else {
-            product.attributes.quantity = quantity;
-            items = [...items, product];
-        }
-        setCartItems(items);
-    };
-
-    const handleRemoveFromCart = (product) => {
-        let items = [...cartItems];
-        items = items?.filter((p) => p.id !== product?.id);
-        setCartItems(items);
-    };
-
-    const handleCartProductQuantity = (type, product) => {
-        let items = [...cartItems];
-        let index = items?.findIndex((p) => p.id === product?.id);
-        if (type === "inc") {
-            items[index].attributes.quantity += 1;
-        } else if (type === "dec") {
-            if (items[index].attributes.quantity === 1) return;
-            items[index].attributes.quantity -= 1;
-        }
-        setCartItems(items);
-    };
-
+function App() {
     return (
-        <Context.Provider
-            value={{
-                products,
-                setProducts,
-                categories,
-                setCategories,
-                cartItems,
-                setCartItems,
-                handleAddToCart,
-                cartCount,
-                handleRemoveFromCart,
-                showCart,
-                setShowCart,
-                handleCartProductQuantity,
-                cartSubTotal,
-            }}
-        >
-            {children}
-        </Context.Provider>
+        <BrowserRouter>
+            <AppContext>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/category/:id" element={<Category />} />
+                    <Route path="/product/:id" element={<SingleProduct />} />
+                </Routes>
+                <Newsletter />
+                <Footer />
+            </AppContext>
+        </BrowserRouter>
     );
-};
+}
 
-export default AppContext;
+export default App;
